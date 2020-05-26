6b44ba032087abacd1aafadf1896395a1ab7e896
Wellington Lourenço Borges <53665329+wlborges@users.noreply.github.com>
2020-05-26 20:25:53 -0300

    Update SendController.js

--- Controller/SendController.js
+++ Controller/SendController.js
@@ -5,11 +5,11 @@ module.exports = {
         
         let { to, subject, html  } = request.body;
         const transporter = nodemailer.createTransport({
-            host: 'smtp.live.com',
+            //host: 'smtp.live.com',
+            //port: 25,
+            host: 'mail.wlborges.com',
             port: 465,
             auth: {
-                //user : "amigochocolate@outlook.com.br",
-                //pass : "Amigo@123"
                 user : "noreply@wlborges.com",
                 pass : "1a2QK7zIM=eh"
             }
